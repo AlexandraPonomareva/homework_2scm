01438370fec7b6ebd92d42b470ffe00be82efa56
AlexandraPonomareva <62014268+AlexandraPonomareva@users.noreply.github.com>
2023-06-07 20:12:56 +0300

    Update Task_1_hello.c
    Remade task 4 with adding intermediate files to

--- Lab_1/Task_1_hello.c
+++ Lab_1/Task_1_hello.c
@@ -1,6 +1,14 @@
-﻿#include <stdio.h>
+#include <stdio.h>
+#include <locale.h>
 
-int main (int narg, char* arg[]){
-    printf ("Hello, %s!\n", arg[1]);
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "Rus");
+    
+    if (argc > 1) {
+        printf("Hello, %s!\n", argv[1]);
+    } else {
+        printf("Hello, World!\n");
+    }
     return 0;
 }
